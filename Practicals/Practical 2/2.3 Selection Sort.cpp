509555cecf9a8a6e85695ffb8b28881ad2e6f4d8
SakshiBodke01 <sakshibodke0104@gmail.com>
2026-01-27 10:10:35 +0530

    Add files via upload

---
+++ Practicals/Practical 2/2.3 Selection Sort.cpp
@@ -0,0 +1,27 @@
+//Selection Sort
+#include <iostream>
+using namespace std;
+int main() {
+    int n, a[50];
+    cout << "Enter number of elements: ";
+    cin >> n;
+    cout << "Enter elements:\n";
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+
+        for (int j = i + 1; j < n; j++) {
+            if (a[j] < a[minIndex])
+                minIndex = j;
+        }
+        int temp = a[i];
+        a[i] = a[minIndex];
+        a[minIndex] = temp;
+    }
+    cout << "Sorted array:\n";
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " ";
+    return 0;
+}
+
